9aa342ef345a98c9ea4e3ad17e32a7943d8b1c3e
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2018-07-24 10:33:08 +0200

    refactoring

--- app/app.py
+++ app/app.py
@@ -1,18 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
 from pymongo import MongoClient
 from bson.json_util import dumps
 import datetime
 
 app = Flask(__name__)
-client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'],27017)
 
-#client = MongoClient("localhost",27017) 
+client = MongoClient(os.environ.get('DB')) 
 db = client.blog
 
-@app.route('/posts')
+@app.route('/')
 def home():
-    return jsonify("please visit this url to read inserted blog post")
+    return jsonify("please visit this url to read inserted blog post : http://0.0.0.0:5000/posts")
 
 @app.route('/posts')
 def posts():
